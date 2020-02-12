425e2973bedd953a7fa8bed2a462474c5397ef2a
ArtemSubbotin <subbotin@mercurydevelopment.com>
2020-02-12 18:08:36 +0400

    Updates

---
+++ precache-manifest.0251a528a6a7ee339537cf4f59c1630d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10190a1231f709917a73a6fc4772272f",
+    "url": "/react-global-site/index.html"
+  },
+  {
+    "revision": "ad40513c648bac44d4ee",
+    "url": "/react-global-site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5563639aa0e10e6cc3bc",
+    "url": "/react-global-site/static/js/2.4f4fd390.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-global-site/static/js/2.4f4fd390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad40513c648bac44d4ee",
+    "url": "/react-global-site/static/js/main.8c3d8323.chunk.js"
+  },
+  {
+    "revision": "ed4a76cd1bd532362d14",
+    "url": "/react-global-site/static/js/runtime-main.5ce8253c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-global-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
